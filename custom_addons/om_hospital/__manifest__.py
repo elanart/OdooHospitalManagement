13b47e1c275c6b1aa51e9e6d9566c042c9a652d1
Lan <elanart.06@gmail.com>
2025-03-01 13:31:20 +0700

    Initial commit

---
+++ custom_addons/om_hospital/__manifest__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Hospital Management",
+    'summary': """Hospital Management""",
+    'description': """Managing hospital information""",
+    'version':"",
+    'author': "Tran Le Lan",
+    'website': "",
+    'category': 'Uncategorized',
+    'version': '0.1',
+    'depends': [
+        'base',
+    ],
+    'data': [],
+    # 'qweb': ['static/src/xml/*.xml'],
+    'installable': True,
+    'application': True,
+}
